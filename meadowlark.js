eda7f26ef16f6470d2a716ad8e7a4cb5051a4b30
zx8086 <1085655+zx8086@users.noreply.github.com>
2022-07-22 17:35:58 +0200

    Partials + Newsletter

--- meadowlark.js
+++ meadowlark.js
@@ -1,20 +1,42 @@
 const express = require('express')
+const app = express()
+
 const expressHandlebars = require('express-handlebars')
 
 const handlers = require('./lib/handlers')
+const weatherMiddlware = require('./lib/middleware/weather')
 
-// const fortune = require('./lib/fortune.js')
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
 
-const app = express()
+const fortune = require('./lib/fortune.js')
 
-app.engine('handlebars', expressHandlebars.engine({ defaultLayout: 'main' }))
+app.engine('handlebars', expressHandlebars.engine({
+  defaultLayout: 'main',
+  helpers: {
+    section: function(name, options) {
+      if(!this._sections) this._sections = {}
+      this._sections[name] = options.fn(this)
+      return null
+    },
+  },
+}))
 app.set('view engine', 'handlebars')
 
 app.use(express.static(__dirname + '/public'))
 
+app.use(weatherMiddlware)
+
 // const port = process.env.PORT || 3010
 
 app.get('/', handlers.home)
+
+// handlers for browser-based form submission
+app.get('/newsletter-signup', handlers.newsletterSignup)
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
+
+app.get('/section-test', handlers.sectionTest)
 
 app.get('/about', handlers.about)
 
